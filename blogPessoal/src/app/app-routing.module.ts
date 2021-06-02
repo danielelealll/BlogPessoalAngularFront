0506cc8b723ab3da07b68ba0d06d12632e8ea3d7
unknown <danieleleal4@gmail.com>
2021-06-01 23:37:21 -0300

    Consumo de API - Login e Cadastro

--- blogPessoal/src/app/app-routing.module.ts
+++ blogPessoal/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { EntrarComponent } from './entrar/entrar.component';
 import { CadastrarComponent } from './cadastrar/cadastrar.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
 
 
 
@@ -10,7 +11,9 @@ const routes: Routes = [
   {path:'', redirectTo: 'entrar', pathMatch: 'full'},
 
   {path:'entrar', component:EntrarComponent},
-  {path: 'cadastrar', component:CadastrarComponent}
+  {path: 'cadastrar', component:CadastrarComponent},
+  
+  {path: 'inicio', component: InicioComponent}
 
 ];
 
